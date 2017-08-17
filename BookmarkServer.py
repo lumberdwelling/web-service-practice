79d86a7050caae8de2a56528247ab71bdbad4164
Patrick Koidal <pkoidal519@gmail.com>
2017-08-17 17:44:38 -0500

    config file stuff

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -3,6 +3,7 @@
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
+import os
 
 memory = {}
 
@@ -96,6 +97,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
